eef8d789caf6663a153ea56b4ae56272354f6f0d
denis1222223 <denis1222223@mail.ru>
2016-11-09 12:22:54 +0300

    Fix lazy evaluation

--- Task2-Javascript/src/lazyEvaluation.js
+++ Task2-Javascript/src/lazyEvaluation.js
@@ -1,30 +1,13 @@
 define(['../src/bind'], function() {
-
+    
     function lazyEvaluation (func) {
-        
-        var bindedLazyEvaluation = bind.apply(null, arguments);
-        var noArg;
-        var storage = [];
-        
-        function isExists(arg) {
-            if (!arg) {
-                if (!noArg) {
-                    noArg = bindedLazyEvaluation(); 
-                }
-                return false;
+        var bindedFunc = bind.apply(null, arguments);
+        var result;
+        return function() {
+            if (result != undefined) {
+                return result;
             }
-            return true;
-        }
-        
-        return function(arg) { 
-            if (isExists(arg)) {
-                if (!(arg in storage)) {
-                    storage[arg] = bindedLazyEvaluation(arg);
-                }   
-                return storage[arg];
-            } else {
-                return noArg;
-            }
+            return result = bindedFunc();
         }
     }
     
--- Task2-Javascript/tests/lazyEvaluation.Test.js
+++ Task2-Javascript/tests/lazyEvaluation.Test.js
@@ -1,49 +1,52 @@
 define(['test-functions-lib', '../src/lazyEvaluation'], function(testLib, lazyEvaluation) {
 
     var counter = 0;
-    var counterEmptyParams = 0;
     
     function customSum() {
         counter++;
         return testLib.sum.apply(null, arguments);
     }
     
-    function customSumEmptyParams() {
-        counterEmptyParams++;
-        return testLib.sum.apply(null, arguments);
-    }
-    
     var lazySumTwoAndThree = lazyEvaluation(customSum, 2, 3);
-    var lazySumTwoAndThreeEmpty = lazyEvaluation(customSumEmptyParams, 2, 3);     
-    var lazyMultFive = lazyEvaluation(testLib.mult, 5);
+    var lazySumTwoAndFive = lazyEvaluation(customSum, 2, 5)
+    var lazySumTwoAndUndefined = lazyEvaluation(customSum, 2, undefined);
     
     describe('lazy evaluation', function () {
 
-        it('lazy evaluation of sum 2 + 3 + 2', function () {
-            expect(lazySumTwoAndThree(2)).toBe(7);
+        it('lazy evaluation of sum 2 + 3', function () {
+            expect(lazySumTwoAndThree()).toBe(5);
         });
-        
-        it('3 times more of sum 2 + 3 + 2', function () {
-            lazySumTwoAndThree(2);
-            lazySumTwoAndThree(2);
-            lazySumTwoAndThree(2);
-            
+
+        it('3 times more of sum 2 + 3', function () {
+            lazySumTwoAndThree();
+            lazySumTwoAndThree(5);
+            lazySumTwoAndThree(6, 7);
+
             expect(counter).toBe(1);
         });
-        
-        it('lazy evaluation of sum 2 and 3 and no more params', function () {
-            expect(lazySumTwoAndThreeEmpty()).toBe(5);
+
+        it('lazy evaluation of sum 2 + 5', function () {
+            expect(lazySumTwoAndFive()).toBe(7);
         });
-        
-        it('2 times more of sum 2 + 3 and no more params', function () {
-            lazySumTwoAndThreeEmpty();
-            lazySumTwoAndThreeEmpty();
-            
-            expect(counterEmptyParams).toBe(1);
+
+        it('3 times more of sum 2 + 5', function () {
+            lazySumTwoAndFive();
+            lazySumTwoAndFive(33);
+            lazySumTwoAndFive();
+
+            expect(counter).toBe(2);
         });
-        
-        it('lazy evaluation of mult 5 and 4', function () {
-            expect(lazyMultFive(4)).toBe(20);
+
+        it('lazy evaluation of sum 2 + undefined', function () {
+            expect(lazySumTwoAndUndefined()).toEqual(NaN);
+        });
+
+        it('3 times more of sum 2 + undefined', function () {
+            lazySumTwoAndUndefined();
+            lazySumTwoAndUndefined();
+            lazySumTwoAndUndefined();
+
+            expect(counter).toBe(3);
         });
         
     });
