0d183a704e16bab04f35261137ba1a2a563ffc9c
denis1222223 <denis1222223@mail.ru>
2016-11-12 15:41:12 +0300

    Fix identification of special outputs for special buttons

--- Task3-Javascript-jQuery/AjaxRequestViewer/src/ajaxRequestViewer.js
+++ Task3-Javascript-jQuery/AjaxRequestViewer/src/ajaxRequestViewer.js
@@ -1,103 +1,59 @@
-// (function($) {
-//
-//     $.fn.audioPlayer = function(options) {
-//
-//         var settings = $.extend( {
-//             'name'         : 'The Times They Are Changin',
-//             'author'       : 'Bob Dylan',
-//             'src'          : 'music/bob_dylan_the_times_they_are_a_changin.mp3'
-//         }, options);
-//
-//
-//
-//         return this;
-//     };
-//
-// })(jQuery);
-
 $( function() {
     $.widget( "custom.ajaxRequestViewer", {
 
         options: {
+            requestSettings: {
+                cache: false
+            },
+            guiSettings: {
+                output: $("body"),
+                color: "#62b4e8",
+                backgroundColor: "#c1c1c1",
+                width: "100px",
+                height: "20px"
+            }
+        },
 
+        send: function() {
+            $.ajax(this.options.requestSettings);
+        },
+
+        setMarkup: function() {
+            var progressBar = $("<div class='progressBar'>" +
+                "<div class='progress'></div>" +
+                "</div>");
+
+            progressBar.width(this.options.guiSettings.width);
+            progressBar.height(this.options.guiSettings.height);
+            progressBar.css("background-color", this.options.guiSettings.backgroundColor);
+            progressBar.find(".progress").css("background-color", this.options.guiSettings.color);
+
+            this.options.guiSettings.output.append(progressBar);
+        },
+
+        setRequestHandler: function() {
+            var output = this.options.guiSettings.output;
+
+            this.options.requestSettings.xhr = function() {
+                var xhr = $.ajaxSettings.xhr();
+                xhr.addEventListener('progress', function(evt) {
+                    if (evt.lengthComputable) {
+                        var percentComplete = Math.ceil(evt.loaded / evt.total * 100);
+                        output.find(".progress").width(percentComplete + "%");
+                    }
+                }, false);
+                return xhr;
+            }
         },
 
         _create: function() {
-            
-            // this.element
-            // // add a class for theming
-            //     .addClass( "custom-colorize" );
-            //
-            // this.changer = $( "<button>", {
-            //     text: "change",
-            //     "class": "custom-colorize-changer"
-            // })
-            //     .appendTo( this.element )
-            //     .button();
-            //
-            // // Bind click events on the changer button to the random method
-            // this._on( this.changer, {
-            //     // _on won't call random when widget is disabled
-            //     click: "random"
-            // });
-            // this._refresh();
-        },
+            this.setMarkup();
 
-        // // Called when created, and later when changing options
-        // _refresh: function() {
-        //     this.element.css( "background-color", "rgb(" +
-        //         this.options.red +"," +
-        //         this.options.green + "," +
-        //         this.options.blue + ")"
-        //     );
-        //
-        //     // Trigger a callback/event
-        //     this._trigger( "change" );
-        // },
+            this.element.on( "click", function() {
+                $(this).ajaxRequestViewer("send");
+            });
 
-        // // A public method to change the color to a random value
-        // // can be called directly via .colorize( "random" )
-        // random: function( event ) {
-        //     var colors = {
-        //         red: Math.floor( Math.random() * 256 ),
-        //         green: Math.floor( Math.random() * 256 ),
-        //         blue: Math.floor( Math.random() * 256 )
-        //     };
-        //
-        //     // Trigger an event, check if it's canceled
-        //     if ( this._trigger( "random", event, colors ) !== false ) {
-        //         this.option( colors );
-        //     }
-        // },
-
-        // // Events bound via _on are removed automatically
-        // // revert other modifications here
-        // _destroy: function() {
-        //     // remove generated elements
-        //     this.changer.remove();
-        //
-        //     this.element
-        //         .removeClass( "custom-colorize" )
-        //         .enableSelection()
-        //         .css( "background-color", "transparent" );
-        // },
-
-        // // _setOptions is called with a hash of all options that are changing
-        // // always refresh when changing options
-        // _setOptions: function() {
-        //     // _super and _superApply handle keeping the right this-context
-        //     this._superApply( arguments );
-        //     this._refresh();
-        // },
-        //
-        // // _setOption is called for each individual option that is changing
-        // _setOption: function( key, value ) {
-        //     console.log("colorise(option, colorsObj)");
-        //     // prevent invalid color values
-        //     if ( /red|green|blue/.test(key) && (value < 0 || value > 255) ) {
-        //         return;
-        //     }
-        //     this._super( key, value );
-        // }
+            this.setRequestHandler();
+        }
     });
-} );
+});
