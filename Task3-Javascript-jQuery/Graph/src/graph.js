88bdab59c61e32571abe3409ff44454c1b8360a3
denis1222223 <denis1222223@mail.ru>
2016-11-14 16:36:32 +0300

    Add graph tests

--- Task3-Javascript-jQuery/Graph/src/graph.js
+++ Task3-Javascript-jQuery/Graph/src/graph.js
@@ -1,91 +1,44 @@
-function Node(data) {
-    this.data = data;
-    this.parent = null;
-    this.children = [];
+function Graph(matrix) {
+    this.nodesCount = matrix.length;
+    this.relationsMatrix = matrix;
+    this.initialize();
+    this.shortestWays();
 }
 
-function Tree(data) {
-    var node = new Node(data);
-    this._root = node;
+Graph.prototype.initialize = function() {
+    this.distances = [];
+    this.visitedNodes = [];
+    for (var i = 0; i < this.nodesCount; i++) {
+        this.distances[i] = 10000;
+        this.visitedNodes[i] = 1;
+    }
+    this.distances[0] = 0;
 }
 
-Tree.prototype.depthFirstSearch = function(callback) {
-    (function recurse(currentNode) {
-        for (var i = 0, length = currentNode.children.length; i < length; i++) {
-            recurse(currentNode.children[i]);
-        }
-        callback(currentNode);
-    })(this._root);
-};
-
-Tree.prototype.print = function() {
-    var callback = function(node) {
-        console.log(node.data);
-    }
-    this.depthFirstSearch(callback);
+Graph.prototype.shortestWayTo = function(index) {
+    return this.distances[index-1];
 }
 
-Tree.prototype.findNode = function(data) {
-    var nodeToFind;
-    var callback = function(node) {
-        if (node.data === data) {
-            nodeToFind = node;
+Graph.prototype.shortestWays = function() {
+    do {
+        var minindex = 10000;
+        var min = 10000;
+        for (var i = 0; i < this.nodesCount; i++) {
+            if ((this.visitedNodes[i] == 1) && (this.distances[i] < min)) {
+                min = this.distances[i];
+                minindex = i;
+            }
         }
-    }
-    this.depthFirstSearch(callback);
-    return nodeToFind;
-};
-
-Tree.prototype.add = function(data, toData) {
-    var child = new Node(data),
-        parent = null;
-
-    var callback = function(node) {
-        if (node.data === toData) {
-            parent = node;
+        if (minindex != 10000) {
+            for (var i = 0; i < this.nodesCount; i++) {
+                if (this.relationsMatrix[minindex][i] > 0) {
+                    var temp = min + this.relationsMatrix[minindex][i];
+                    if (temp < this.distances[i])
+                        this.distances[i] = temp;
+                }
+            }
+            this.visitedNodes[minindex] = 0;
         }
-    };
-    this.depthFirstSearch(callback);
-
-    if (parent) {
-        parent.children.push(child);
-        child.parent = parent;
-    } else {
-        throw new Error('Cannot add node to a non-existent parent.');
-    }
-};
-
-Tree.prototype.remove = function(data, fromData) {
-    var tree = this,
-        parent = null,
-        index;
-
-    var callback = function(node) {
-        if (node.data === fromData) {
-            parent = node;
-        }
-    };
-    this.depthFirstSearch(callback);
-
-    if (parent) {
-        index = findIndex(parent.children, data);
-
-        if (index === undefined) {
-            throw new Error('Node to remove does not exist.');
-        } else {
-            parent.children.splice(index, 1);
-        }
-    } else {
-        throw new Error('Parent does not exist.');
-    }
-};
-
-function findIndex(arr, data) {
-    var index;
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i].data === data) {
-            index = i;
-        }
-    }
-    return index;
+    } while (minindex < 10000);
+    return this.distances;
 }
--- Task3-Javascript-jQuery/Graph/tests/graph.Test.js
+++ Task3-Javascript-jQuery/Graph/tests/graph.Test.js
@@ -1,29 +1,26 @@
-describe('tree tests', function () {
+describe('graph tests', function () {
 
-    var tree = new Tree(5);
-    tree.add(4, 5);
-    tree.add(6, 5);
-    tree.add(9, 5);
-    tree.add(1, 9);
+    var matrix = [
+        [0, 7, 9, 0, 0, 14],
+        [7, 0, 10, 15, 0, 0],
+        [9, 10, 0, 11, 0, 2],
+        [0, 15, 11, 0, 6, 0],
+        [0, 0, 0, 6, 0, 9],
+        [14, 0, 2, 0, 9, 0]
+    ]
 
-    it('find node', function () {
-        expect(tree.findNode(6)).toBe(tree._root.children[1]);
+    var graph = new Graph(matrix);
+
+    it('find shortest way from 1th to 1th node', function () {
+        expect(graph.shortestWayTo(1)).toBe(0);
     });
 
-    it('find root node', function () {
-        expect(tree.findNode(5)).toBe(tree._root);
+    it('find shortest way from 1th to 3th node', function () {
+        expect(graph.shortestWayTo(3)).toBe(9);
     });
 
-    it('remove node', function () {
-        tree.remove(9, 5);
-        expect(tree.findNode(9)).toBe(undefined);
-    });
-
-    it('depth first search', function () {
-        tree.depthFirstSearch(function(node) {
-            node.data++;
-        });
-        expect(tree._root.data).toBe(6);
+    it('find shortest way from 1th to 6th node', function () {
+        expect(graph.shortestWayTo(6)).toBe(11);
     });
     
 });
