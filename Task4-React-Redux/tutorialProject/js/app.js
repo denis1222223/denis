e4baed5902621e82532e96fdf356c9e1cfbb20f1
denis1222223 <denis1222223@mail.ru>
2016-11-16 10:02:18 +0300

    Make news add form

--- Task4-React-Redux/tutorialProject/js/app.js
+++ Task4-React-Redux/tutorialProject/js/app.js
@@ -1,3 +1,5 @@
+window.ee = new EventEmitter();
+
 var my_news = [
     {
         author: 'Саша Печкин',
@@ -16,82 +18,9 @@ var my_news = [
     }
 ];
 
-// var Add = React.createClass({
-//     getInitialState: function() { //устанавливаем начальное состояние (state)
-//         return {
-//             agreeNotChecked: true,
-//             authorIsEmpty: true,
-//             textIsEmpty: true
-//         };
-//     },
-//     componentDidMount: function() {
-//         ReactDOM.findDOMNode(this.refs.author).focus();
-//     },
-//     onBtnClickHandler: function(e) {
-//         e.preventDefault();
-//         var author = ReactDOM.findDOMNode(this.refs.author).value;
-//         var text = ReactDOM.findDOMNode(this.refs.text).value;
-//         alert(author + '\n' + text);
-//     },
-//     onCheckRuleClick: function(e) {
-//         this.setState({agreeNotChecked: !this.state.agreeNotChecked}); //устанавливаем значение в state
-//     },
-//     onAuthorChange: function(e) {
-//         if (e.target.value.trim().length > 0) {
-//             this.setState({authorIsEmpty: false})
-//         } else {
-//             this.setState({authorIsEmpty: true})
-//         }
-//     },
-//     onTextChange: function(e) {
-//         if (e.target.value.trim().length > 0) {
-//             this.setState({textIsEmpty: false})
-//         } else {
-//             this.setState({textIsEmpty: true})
-//         }
-//     },
-//     render: function() {
-//         var agreeNotChecked = this.state.agreeNotChecked,
-//             authorIsEmpty = this.state.authorIsEmpty,
-//             textIsEmpty = this.state.textIsEmpty;
-//         return (
-//             <form className='add cf'>
-//                 <input
-//                     type='text'
-//                     className='add__author'
-//                     onChange={this.onAuthorChange}
-//                     placeholder='Ваше имя'
-//                     ref='author'
-//                 />
-//         <textarea
-//             className='add__text'
-//             onChange={this.onTextChange}
-//             placeholder='Текст новости'
-//             ref='text'
-//         ></textarea>
-//                 <label className='add__checkrule'>
-//                     <input type='checkbox' ref='checkrule' onChange={this.onCheckRuleClick}/>Я согласен с правилами
-//                 </label>
-//
-//                 <button
-//                     className='add__btn'
-//                     onClick={this.onBtnClickHandler}
-//                     ref='alert_button'
-//                     disabled={agreeNotChecked || authorIsEmpty || textIsEmpty}
-//                 >
-//                     Показать alert
-//                 </button>
-//             </form>
-//         );
-//     }
-// });
-
 var Add = React.createClass({
-    getInitialState: function() { //устанавливаем начальное состояние (state)
+    getInitialState: function() {
         return {
-            agreeNotChecked: true,
-            authorIsEmpty: true,
-            textIsEmpty: true
         };
     },
     componentDidMount: function() {
@@ -101,28 +30,18 @@ var Add = React.createClass({
         e.preventDefault();
         var author = ReactDOM.findDOMNode(this.refs.author).value;
         var text = ReactDOM.findDOMNode(this.refs.text).value;
-        alert(author + '\n' + text);
-    },
-    onCheckRuleClick: function(e) {
-        ReactDOM.findDOMNode(this.refs.alert_button).disabled = !e.target.checked; //устанавливаем значение в state
-    },
-    onAuthorChange: function(e) {
-        if (e.target.value.trim().length > 0) {
-            this.setState({authorIsEmpty: false})
-        } else {
-            this.setState({authorIsEmpty: true})
-        }
-    },
-    onTextChange: function(e) {
-        if (e.target.value.trim().length > 0) {
-            this.setState({textIsEmpty: false})
-        } else {
-            this.setState({textIsEmpty: true})
-        }
+
+        var item = [{
+            author: author,
+            text: text,
+            bigText: '...'
+        }];
+
+        window.ee.emit('News.add', item);
     },
     render: function() {
         return (
-            <form className="add cf">
+            <form className="add">
                 <input
                     type='text'
                     className='add__author'
@@ -134,22 +53,13 @@ var Add = React.createClass({
                     placeholder='Текст новости'
                     ref='text'
                 />
-                <label className='add__checkrule'>
-                    <input
-                        type='checkbox'
-                        ref='checkrule'
-                        onChange={this.onCheckRuleClick}
-                    />
-                    Я согласен с правилами
-                </label>
-                <button
-                    className='add__btn'
+                <a href="#"
+                    className='add__news'
                     onClick={this.onBtnClickHandler}
                     ref='alert_button'
-                    disabled
                 >
-                    Показать alert
-                </button>
+                    Добавить новость
+                </a>
             </form>
         );
     }
@@ -209,12 +119,27 @@ var News = React.createClass({
 });
 
 var App = React.createClass({
+    getInitialState: function() {
+        return {
+            news: my_news
+        };
+    },
+    componentDidMount: function() {
+        var self = this;
+        window.ee.addListener('News.add', function(item) {
+            var nextNews = item.concat(self.state.news);
+            self.setState({news: nextNews});
+        });
+    },
+    componentWillUnmount: function() {
+        window.ee.removeListener('News.add');
+    },
     render: function() {
         return (
             <div className="app">
                 <h3>Новости</h3>
                 <Add />
-                <News data={my_news} />
+                <News data={this.state.news} />
             </div>
         );
     }
