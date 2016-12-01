88a511622fb407595525752c80daafe30a178f1e
denis1222223 <denis1222223@mail.ru>
2016-12-01 14:05:40 +0300

    add task buttons

--- Task4-React-Redux/SprintsProject/src/components/TaskShortcut/TaskShortcut.js
+++ Task4-React-Redux/SprintsProject/src/components/TaskShortcut/TaskShortcut.js
@@ -1,6 +1,8 @@
 import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router';
 import Panel  from 'react-bootstrap/lib/Panel';
+import Button  from 'react-bootstrap/lib/Button';
+import Glyphicon  from 'react-bootstrap/lib/Glyphicon';
 
 import './taskShortcut.less';
 
@@ -17,6 +19,12 @@ class TaskShortcut extends Component {
                     <Link to={"/tasks?id=" + task.id}>
                         <Panel>
                             {task.name}
+                            <Button className="editTaskButton" bsSize="xsmall" bsStyle="warning">
+                                <Glyphicon glyph="glyphicon glyphicon-edit" />
+                            </Button>
+                            <Button className="deleteTaskButton" bsSize="xsmall" bsStyle="danger">
+                                <Glyphicon glyph="glyphicon glyphicon-trash" />
+                            </Button>
                         </Panel>
                     </Link>
 
