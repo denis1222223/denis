b17a44cd7726bbf6d2789a02048cae7c83b02ecd
Denis Al-Khelali <denis.al-khelali@itechart-group.com>
2017-01-30 02:23:22 +0300

    config webpack+react starter kit

---
+++ Task6-WebComponents/react/webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var ExtractTextPlugin  = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'assets'),
+        publicPath: "/assets"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: { presets: ['react', 'es2015'] }
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader!less-loader')
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ],
+    devServer: {
+        inline: true
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
